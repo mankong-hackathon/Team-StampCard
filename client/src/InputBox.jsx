d437ba55fb96d5018ba833c26a2db615f2c768ff
Chen Li <yichen.li0830@gmail.com>
2017-06-24 14:17:07 -0700

    Initializing InputBox component

---
+++ client/src/InputBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {compose, withState, withHandlers} from 'recompose';
+import { Tag } from 'antd';
+
+
+const InputBox = (props) => {
+  return <div>
+    inputbox
+  </div>
+};
+
+export default InputBox;
