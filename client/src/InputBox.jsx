8c18e959435802f7e3bd6375a94056c22cbe767b
Chen Li <yichen.li0830@gmail.com>
2017-06-24 15:55:49 -0700

    Implement InputBox component

--- client/src/InputBox.jsx
+++ client/src/InputBox.jsx
@@ -1,12 +1,115 @@
 import React from 'react';
 import {compose, withState, withHandlers} from 'recompose';
 import { Tag } from 'antd';
+const CheckableTag = Tag.CheckableTag;
+import { Row, Col } from 'antd';
 
+
+const styles = {
+  boxWrapper: {
+    webkitBoxShadow: "0px 0px 22px -1px rgba(214,214,214,1)",
+    mozBoxShadow: "0px 0px 22px -1px rgba(214,214,214,1)",
+    boxShadow: "0px 0px 54px 3px rgba(237,237,237,1)",
+    background: '#fff',
+    borderRadius: 5,
+    height: "50vh",
+    width: "90vw",
+    marginTop: 50,
+    marginLeft: "auto",
+    marginRight: "auto",
+    padding: 10,
+    overflow: "auto",
+  },
+  rightCol: {
+    minHeight: "47vh",
+    border: "solid #eee",
+    borderWidth: "0 0 0 1px",
+    padding: "10px",
+  },
+  tag:{
+    marginBottom: 10,
+  },
+  leftCol:{
+    overflow: "auto",
+    minHeight: "40vh",
+  }
+};
+
+const categoryToColors = {
+  "REGULAR" : null,
+  "INTRO": "green",
+  "PROBING": "red",
+  "BENEFITS": "blue",
+  "CREDIBILITY":"orange",
+  "CLOSE": "purple"
+};
 
 const InputBox = (props) => {
-  return <div>
-    inputbox
+  const tags = props.finalTextList.map((finalText,i) =>
+      <Tag style={styles.tag}
+           key={i}
+           color={categoryToColors[finalText.category]}>
+        {finalText.text}
+      </Tag>
+  );
+  return <div style={styles.boxWrapper}>
+    <Row>
+      <Col span={18} style={styles.leftCol}>
+        {tags}
+        {
+          props.tempText.length > 0
+          &&
+          <CheckableTag>
+            {props.tempText}
+          </CheckableTag>
+        }
+      </Col>
+      <Col span={6} style={styles.rightCol}>
+        <Row>
+          <Tag style={styles.tag}
+               color={"green"}>
+            Introduction
+          </Tag>
+        </Row>
+        
+        <Row>
+          <Tag style={styles.tag}
+               color={"orange"}>
+            Credibility
+          </Tag>
+        </Row>
+        
+        <Row>
+          <Tag style={styles.tag}
+               color={"red"}>
+            Probing question
+          </Tag>
+        </Row>
+        
+        <Row>
+          <Tag style={styles.tag}
+               color={"blue"}>
+            Product Benefits
+          </Tag>
+        </Row>
+        
+        <Row>
+          <Tag style={styles.tag}
+               color={"purple"}>
+            Attempts to close deal
+          </Tag>
+        </Row>
+        
+        <Row>
+          <Tag style={styles.tag}>
+            Regular
+          </Tag>
+        </Row>
+      
+      </Col>
+    </Row>
   </div>
+  
 };
 
 export default InputBox;
